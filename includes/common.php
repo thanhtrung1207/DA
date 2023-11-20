61357cf69ec4eee04c83ef93e873532fa1898496
thanhtrung <92674174+thanhtrung1207@users.noreply.github.com>
2023-11-20 18:23:00 +0700

    Delete includes directory

--- includes/common.php
+++
@@ -1,7 +0,0 @@
-<!--establish the connection to database, and start the session-->
-<?php
-$con = mysqli_connect("localhost", "root", "", "store")or die($mysqli_error($con));
-if(session_status() == PHP_SESSION_NONE){
-    session_start();
-}
-?>
